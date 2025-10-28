e9a4d3fb5e14d3c8abd32644958e84ce0f97ba34
Dennis Chinecherem <densenj@outlook.com>
2025-10-28 21:46:28 +0100

    update parent class shape

--- shape.py
+++ shape.py
@@ -1,21 +1,42 @@
-#GEOMETRY: Parent class; Shape
-# Shape comparison (for sub-classes;rectangle and circle, by size).
+#GEOMETRY: Parent class; Shape( represents/declares what every shape must have. )
+# Shape comparison (for sub-classes;rectangle and circle, by area).
 #Translation:... ..movement of the shapes(rectangle and circle from their various center position (0,0))
+import math  #(built in for mathematical functions and calculations)
+import matplotlib.pyplot as py
+
 
 class Shape:
     """Parent class for the geometric shapes
     """
 
     """attributes x and y coordinates for the shapes """
-    def __int__(self, x:int|float , y:int|float):
-        self.x=x
-        self.y=y
+    def __int__(self, x , y):
+
+        """initialize a shape with x and y cordinates and raises type error if x and y are not numeric(error check)"""
+
+        if not isinstance (x,(int,float)) and isinstance(y,(int,float)):
+            raise TypeError("x and y must be numeric values.")
+        self._x=x
+        self._y=y
 
 
     @property
     def x(self):
+        """returns the x coordinate of the shape"""
         return self._x 
     
+    @property
+    def y(self):
+        """returns the y coordinate of the shape"""
+        return self._y
+    
+
+    def translate(self, dx, dy):
+        """move the shape by dx and dy and raises type error if they are not numeric values"""
+        if not isinstance (dx,(int,float)) and isinstance (dy,(int,float)):
+            raise TypeError("dx and dy must be numeric values.")
+        self._x+=dx #(movements occurs when a value/number assigned to dy or dx is added to the x and y instances which is set at 0,0)
+        self._y+=dy
     
 
 
